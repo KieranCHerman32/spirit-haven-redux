e139086851a111c3de5cfb20b8c601555b341a4b
KieranCHerman32 <kc.herman32@gmail.com>
2023-07-15 14:34:30 -0500

    Created Nav (Header), Footer, and Main Content components

--- spirit-haven/src/app/app.module.ts
+++ spirit-haven/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
