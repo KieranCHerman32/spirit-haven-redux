d7f972b4d8383bc7057295f32b07e898ac1f971c
KieranCHerman32 <kc.herman32@gmail.com>
2023-08-04 17:58:13 -0500

    Modified footer layout and positioning.

--- spirit-haven/src/app/app.module.ts
+++ spirit-haven/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgOptimizedImage } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatDividerModule } from '@angular/material/divider';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgOptimizedImage } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
 import { PrincipleDividerComponent } from './principle-divider/principle-divider.component';
 
 @NgModule({
@@ -39,4 +43,8 @@ import { PrincipleDividerComponent } from './principle-divider/principle-divider
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebook, faInstagram);
+  }
+}
