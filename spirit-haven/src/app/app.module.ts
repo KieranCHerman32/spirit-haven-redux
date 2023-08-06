8e614831ddfb58036104d9a81f4ebcfa2435a7bb
KieranCHerman32 <kc.herman32@gmail.com>
2023-08-06 04:54:37 -0500

    Added TODO for list content

--- spirit-haven/src/app/app.module.ts
+++ spirit-haven/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
