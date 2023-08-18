81a998fe1904e949c12aa8d575fc6adc3cdb8d30
KieranCHerman32 <kc.herman32@gmail.com>
2023-08-18 17:41:29 -0500

    Began building carousel content

--- spirit-haven/src/app/carousel/carousel.component.ts
+++ spirit-haven/src/app/carousel/carousel.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent {
-
+  slides: any[] = new Array(3).fill({
+    id: -1,
+    src: '',
+    title: '',
+    subtitle: '',
+  });
 }
