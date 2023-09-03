3c530d69605a54b9524e59bceb87ff4a5c2a4280
KieranCHerman32 <kc.herman32@gmail.com>
2023-09-03 04:59:53 -0500

    Update carousel.component.ts

--- spirit-haven/src/app/carousel/carousel.component.ts
+++ spirit-haven/src/app/carousel/carousel.component.ts
@@ -12,18 +12,18 @@ export class CarouselComponent {
 
 export class SlideShow {
   static slides: CarouselSlide[] = [
-    // {
-    //   src: '../../assets/jpg/grandmothers-right.jpg',
-    //   title: 'Come home.',
-    //   subtitle:
-    //     'Our 100+ acre ranch is where we come together to celebrate as a community.\nIts name is Spirit Haven Ranch, but we prefer to call it home.',
-    // },
-    // {
-    //   src: '../../assets/jpg/revel-wide.jpg',
-    //   title: 'Find your community.',
-    //   subtitle:
-    //     'Visit for just one Festival or WorkEnd and make connections that last a lifetime.',
-    // },
+    {
+      src: '../../assets/jpg/grandmothers-right.jpg',
+      title: 'Come home.',
+      subtitle:
+        'Our 100+ acre ranch is where we come together to celebrate as a community.\nIts name is Spirit Haven Ranch, but we prefer to call it home.',
+    },
+    {
+      src: '../../assets/jpg/revel-wide.jpg',
+      title: 'Find your community.',
+      subtitle:
+        'Visit for just one Festival or WorkEnd and make connections that last a lifetime.',
+    },
     // PROBLEM CHILD #1
     {
       src: '../../assets/jpg/guardian-fire-dark.jpg',
@@ -32,17 +32,17 @@ export class SlideShow {
         'Take a break from the mundane world and immerse yourself in a Pagan-centric space where you can fully be you.',
     },
     // PROBLEM CHILD #2
-    // {
-    //   src: '../../assets/jpg/altar.jpg',
-    //   title: 'Discover your path.',
-    //   subtitle:
-    //     'Spirit Haven is made up of rich, diverse practices and traditions. \nGo to a workshop or strike up a conversation with just about anyone to learn something new.',
-    // },
-    // {
-    //   src: '../../assets/jpg/lothlorien_path.jpg',
-    //   title: 'A haven for your spirit.',
-    //   subtitle:
-    //     'A multicultural Pagan organization built on a foundation of love, community, and passion.',
-    // },
+    {
+      src: '../../assets/jpg/altar.jpg',
+      title: 'Discover your path.',
+      subtitle:
+        'Spirit Haven is made up of rich, diverse practices and traditions. \nGo to a workshop or strike up a conversation with just about anyone to learn something new.',
+    },
+    {
+      src: '../../assets/jpg/lothlorien_path.jpg',
+      title: 'A haven for your spirit.',
+      subtitle:
+        'A multicultural Pagan organization built on a foundation of love, community, and passion.',
+    },
   ];
 }
