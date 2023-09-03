b6084428359feebddfb8d29edd2cd18c79e9473f
KieranCHerman32 <kc.herman32@gmail.com>
2023-09-03 00:25:19 -0500

    Restoring commented content

--- spirit-haven/src/app/carousel/carousel.component.ts
+++ spirit-haven/src/app/carousel/carousel.component.ts
@@ -18,29 +18,29 @@ export class SlideShow {
       subtitle:
         'Our 100+ acre ranch is where we come together to celebrate as a community. Its name is Spirit Haven Ranch, but we prefer to call it home.',
     },
-    // {
-    //   src: '../../assets/jpg/revel-wide.jpg',
-    //   title: 'Find your community.',
-    //   subtitle:
-    //     'Visit for just one Festival or WorkEnd and make connections that last a lifetime.',
-    // },
-    // {
-    //   src: '../../assets/jpg/guardian-fire-dark.jpg',
-    //   title: 'Rekindle your spark.',
-    //   subtitle:
-    //     'Take a break from the mundane world and immerse yourself in a Pagan-centric space where you can fully be you.',
-    // },
-    // {
-    //   src: '../../assets/jpg/altar.jpg',
-    //   title: 'Discover your path.',
-    //   subtitle:
-    //     'Spirit Haven is made up of rich, diverse practices and traditions. Go to a workshop or strike up a conversation with just about anyone to learn something new.',
-    // },
-    // {
-    //   src: '../../assets/jpg/lothlorien_path.jpg',
-    //   title: 'A haven for your spirit.',
-    //   subtitle:
-    //     'A multicultural Pagan organization built on a foundation of love, community, and passion.',
-    // },
+    {
+      src: '../../assets/jpg/revel-wide.jpg',
+      title: 'Find your community.',
+      subtitle:
+        'Visit for just one Festival or WorkEnd and make connections that last a lifetime.',
+    },
+    {
+      src: '../../assets/jpg/guardian-fire-dark.jpg',
+      title: 'Rekindle your spark.',
+      subtitle:
+        'Take a break from the mundane world and immerse yourself in a Pagan-centric space where you can fully be you.',
+    },
+    {
+      src: '../../assets/jpg/altar.jpg',
+      title: 'Discover your path.',
+      subtitle:
+        'Spirit Haven is made up of rich, diverse practices and traditions. Go to a workshop or strike up a conversation with just about anyone to learn something new.',
+    },
+    {
+      src: '../../assets/jpg/lothlorien_path.jpg',
+      title: 'A haven for your spirit.',
+      subtitle:
+        'A multicultural Pagan organization built on a foundation of love, community, and passion.',
+    },
   ];
 }
