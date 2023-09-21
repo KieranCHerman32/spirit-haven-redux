de7f007671a1a313ca0ee6eef664cb5a43426ae1
KieranCHerman32 <kc.herman32@gmail.com>
2023-09-21 13:23:29 -0500

    Added Festival data to environment.ts

--- spirit-haven/src/environments/environment.ts
+++ spirit-haven/src/environments/environment.ts
@@ -5,4 +5,10 @@ export const environment = {
   burn_ban_duration: 90,
   burn_ban_notice_url:
     'https://www.co.fayette.tx.us/upload/page/1777/docs/Burn%20Ban%207-13-23.pdf',
+  next_festival: {
+    festival: 'Samhain 2023',
+    theme: 'Masquerade Of The Wild',
+    start_date: '10/19/2023',
+    end_date: '10/22/2023',
+  },
 };
