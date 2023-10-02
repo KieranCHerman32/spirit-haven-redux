6c54383505081512b77dfd709cf8f7ab911012e9
KieranCHerman32 <kc.herman32@gmail.com>
2023-10-02 12:28:22 -0500

    Refactored Navbar styling

--- spirit-haven/src/app/nav/nav.component.ts
+++ spirit-haven/src/app/nav/nav.component.ts
@@ -1,5 +1,4 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
-import { MatMenuTrigger } from '@angular/material/menu';
+import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -10,29 +9,4 @@ import { environment } from 'src/environments/environment';
 export class NavComponent {
   env = environment;
   memberAuth: any;
-
-  @ViewChildren(MatMenuTrigger)
-  triggerArr!: QueryList<MatMenuTrigger>;
-
-  menuOpen(menuTrigger: MatMenuTrigger) {
-    if (!menuTrigger.menuOpen) {
-      this.menuCloseAll(menuTrigger);
-      setTimeout(() => menuTrigger.openMenu(), 100);
-    }
-  }
-
-  menuClose(menuTrigger: MatMenuTrigger) {
-    if (!menuTrigger.menuOpen) {
-      this.menuCloseAll(menuTrigger);
-      menuTrigger.closeMenu();
-    }
-  }
-
-  menuCloseAll(menuTrigger?: MatMenuTrigger) {
-    for (const trigger of this.triggerArr) {
-      if (!menuTrigger || trigger !== menuTrigger) {
-        trigger.closeMenu();
-      }
-    }
-  }
 }
