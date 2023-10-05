b75bd9a367841d769f1426943164730a2be513b2
KieranCHerman32 <kc.herman32@gmail.com>
2023-10-05 12:49:12 -0500

    Building community component

--- spirit-haven/src/app/community/community.component.ts
+++ spirit-haven/src/app/community/community.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
-  styleUrls: ['./community.component.scss']
+  styleUrls: ['./community.component.scss'],
 })
-export class CommunityComponent {
+export class CommunityComponent implements OnInit {
+  private fragment!: string | null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        this.fragment = fragment;
+        document.querySelector(`#${this.fragment}`)!.scrollIntoView();
+      }
+    });
+  }
 }
