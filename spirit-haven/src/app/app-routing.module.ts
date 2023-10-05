edae317784fe5dd87e80ccaa3ed92f8515bc9946
KieranCHerman32 <kc.herman32@gmail.com>
2023-10-05 12:47:40 -0500

    Update app-routing.module.ts

--- spirit-haven/src/app/app-routing.module.ts
+++ spirit-haven/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { CommunityComponent } from './community/community.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'community', component: CommunityComponent },
 ];
 
 @NgModule({
