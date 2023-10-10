0932ae2d315274e1f6f047eca1a8463615b63dc5
KieranCHerman32 <kc.herman32@gmail.com>
2023-10-09 22:26:29 -0500

    Added member and Volunteer triptych component

---
+++ spirit-haven/src/app/member-triptych/member-triptych.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-triptych',
+  templateUrl: './member-triptych.component.html',
+  styleUrls: ['./member-triptych.component.scss']
+})
+export class MemberTriptychComponent {
+
+}
---
+++ spirit-haven/src/app/volunteer-triptych/volunteer-triptych.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-triptych',
+  templateUrl: './volunteer-triptych.component.html',
+  styleUrls: ['./volunteer-triptych.component.scss']
+})
+export class VolunteerTriptychComponent {
+
+}
