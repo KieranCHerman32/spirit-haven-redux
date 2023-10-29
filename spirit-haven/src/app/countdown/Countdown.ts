cf3e3b908782dc0575af87b87d71e821477794fb
KieranCHerman32 <kc.herman32@gmail.com>
2023-10-28 22:24:50 -0500

    Negative display for countdown numbers.

--- spirit-haven/src/app/countdown/Countdown.ts
+++ spirit-haven/src/app/countdown/Countdown.ts
@@ -1,47 +1,54 @@
+import { TimelineComponent } from '../timeline/timeline.component';
+
 export interface TimeComponents {
-  secondsToEnd: string;
-  minutesToEnd: string;
-  hoursToEnd: string;
-  daysToEnd: string;
+  seconds_remaining: string | number;
+  minutes_remaining: string | number;
+  hours_remaining: string | number;
+  days_remaining: string | number;
 }
 
 export function calcDateDiff(end: Date = new Date(2022, 0, 1)): TimeComponents {
   const countdownEnd = end.valueOf();
 
-  const milliSecondsInASecond = 1000;
-  const hoursInADay = 24;
-  const minutesInAnHour = 60;
-  const secondsInAMinute = 60;
+  const ms_in_s = 1000;
+  const hrs_in_day = 24;
+  const mins_in_hr = 60;
+  const secs_in_min = 60;
 
-  const timeDifference = countdownEnd - Date.now();
+  const time_diff = countdownEnd - Date.now();
 
-  const daysToEnd = Math.floor(
-    timeDifference /
-      (milliSecondsInASecond * minutesInAnHour * secondsInAMinute * hoursInADay)
-  )
+  let days_remaining: string | number = Math.floor(
+    time_diff / (ms_in_s * mins_in_hr * secs_in_min * hrs_in_day)
+  );
+
+  let hours_remaining: string | number = Math.floor(
+    (time_diff / (ms_in_s * mins_in_hr * secs_in_min)) % hrs_in_day
+  );
+
+  let minutes_remaining: string | number = Math.floor(
+    (time_diff / (ms_in_s * mins_in_hr)) % secs_in_min
+  );
+
+  let seconds_remaining: string | number =
+    Math.floor(time_diff / ms_in_s) % secs_in_min;
+
+  days_remaining = (days_remaining > 0 ? days_remaining : '0')
+    .toString()
+    .padStart(2, '0');
+  hours_remaining = (hours_remaining > 0 ? hours_remaining : '0')
+    .toString()
+    .padStart(2, '0');
+  minutes_remaining = (minutes_remaining > 0 ? minutes_remaining : '0')
+    .toString()
+    .padStart(2, '0');
+  seconds_remaining = (seconds_remaining > 0 ? seconds_remaining : '0')
     .toString()
     .padStart(2, '0');
 
-  const hoursToEnd = Math.floor(
-    (timeDifference /
-      (milliSecondsInASecond * minutesInAnHour * secondsInAMinute)) %
-      hoursInADay
-  )
-    .toString()
-    .padStart(2, '0');
-
-  const minutesToEnd = Math.floor(
-    (timeDifference / (milliSecondsInASecond * minutesInAnHour)) %
-      secondsInAMinute
-  )
-    .toString()
-    .padStart(2, '0');
-
-  const secondsToEnd = (
-    Math.floor(timeDifference / milliSecondsInASecond) % secondsInAMinute
-  )
-    .toString()
-    .padStart(2, '0');
-
-  return { secondsToEnd, minutesToEnd, hoursToEnd, daysToEnd };
+  return {
+    seconds_remaining,
+    minutes_remaining,
+    hours_remaining,
+    days_remaining,
+  };
 }
